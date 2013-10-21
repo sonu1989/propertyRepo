cc009ff751c9fef99cf7d92643c725acad8ac8e3
sonu1989 <sonu@grepruby.com>
2013-10-21 17:53:17 +0530

    Changes in Application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ end
 
 module Property
   class Application < Rails::Application
+   
+    config.assets.compile = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
